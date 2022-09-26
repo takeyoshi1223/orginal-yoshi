6fbe0a09ad1e4825a10357a0b65bbc9d705062fd
竹田佳史 <yoshifumitakeda@takedayoshifuminoMacBook-Pro.local>
2022-09-27 00:08:51 +0900

    second

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function($) {
+    $('.bg-switcher').bgSwitcher({
+     images:['/images/bg-image1.jpeg','/images/bg-image2.jpeg','/images/bg-image3.jpeg','/images/bg-image4.jpeg','/images/bg-image5.jpeg','/images/bg-image6.jpeg','/images/bg-image7.jpeg'],
+     interval: 5000,
+     loop: true
+    });
+});
